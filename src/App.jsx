fd52e57d817107f02312ef13fd76f062b3a6e532
VasylKostyrko <crocodile21@gmail.com>
2024-08-01 19:05:18 +0300

    Update App.jsx
    Тема

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css'
 import Calculator from './components/Calculator'
-
+import { extendTheme } from '@chakra-ui/react'
+const theme = extendTheme({
+  colors: {
+    capri: '#00C0F9',
+    tomato: '#FF6347',
+  },
+})
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+)
 function App() {
   return (
     <Calculator />
