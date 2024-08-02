748104037c93a2b350073a189ee11a152fcd60d9
VasylKostyrko <crocodile21@gmail.com>
2024-08-02 13:10:33 +0300

    Add build

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,6 @@
 import './App.css'
 import Calculator from './components/Calculator'
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
-import ReactDOM from 'react-dom/client'
-import React from 'react'
 
-const theme = extendTheme({
-  colors: {
-    capri: '#00C0F9',
-    tomato: '#FF6347',
-  },
-})
-ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-)
 function App() {
   return (
     <Calculator />
--- src/components/ClickCalc.jsx
+++ src/components/ClickCalc.jsx
@@ -1,11 +1,12 @@
-import { Box, Flex, Text, Button } from '@chakra-ui/react'
+import { Flex, Text, Button } from '@chakra-ui/react'
 import { useState } from 'react'
 
 function CountButton(props) {
   const expressions = /\+|\-|\/|\*| /
-  const lastNumber = props.data[props.data.length - 1]
+  const lastDigit = props.data[props.data.length - 1]
+
   function checkExpressionType() {
-    if (expressions.test(lastNumber)) return
+    if (expressions.test(lastDigit)) return
 	props.onClick(props.data + props.expression)
   }
   return (
@@ -19,13 +20,17 @@ function Numbers(props) {
   const nums = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'].map(
     number => {
 	  return (
-		  <Button onClick={ (e) => {
-			if (props.data !== '0') props.onClick(props.data + e.target.innerHTML)
-			else props.onClick(e.target.innerHTML)
+		  <Button 
+			onClick={ 
+				(e) => {
+					if (props.data !== '0') 
+						props.onClick(props.data + e.target.innerHTML)
+					else 
+						props.onClick(e.target.innerHTML)
 				}
 			}
 			key={number} w="40px" h="40px" m="4">
-			{number}
+				{number}
 		  </Button>
 	  )
 	}
@@ -36,7 +41,7 @@ function Numbers(props) {
 }
   
  function ClickCalc(props) { 
-   const [counts, setCounts] = useState('0')
+   const [counts, setCounts] = useState('')
    const [result, setResult] = useState('')
    function applyExpression(countedNumber) {
 	 setCounts(countedNumber)
@@ -68,9 +73,17 @@ function Numbers(props) {
 			  <CountButton data={counts} expression={'/'} onClick={applyExpression} />
 			</Flex>
 			<Flex flexDirection="column" marginTop="3">
-			<Button bg="tomato" m="4px" onClick={ () => {setResult(eval(counts))} }>
-			  =
-			</Button>
+				<Button bg="tomato" m="4px" onClick={ () => {
+					setResult(eval(counts))
+				}}>
+				  =
+				</Button>
+				<Button bg="tomato" m="4px" onClick={ () => {
+					setCounts('');
+					setResult('');
+				}}>
+				  C
+				</Button>
 			</Flex>
 		</Flex>
 	</div>
